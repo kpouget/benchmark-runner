4e0177dbcb4d3131249c2c6d0b0299cbf05031c9
Kevin Pouget <kpouget@redhat.com>
2022-03-11 13:05:39 +0100

    benchmark_runner/workloads/mpi_pod: add MPI workload

--- benchmark_runner/main/environment_variables.py
+++ benchmark_runner/main/environment_variables.py
@@ -64,7 +64,7 @@ class EnvironmentVariables:
                                                          'hammerdb_pod_mariadb', 'hammerdb_vm_mariadb', 'hammerdb_kata_mariadb',
                                                          'hammerdb_pod_postgres', 'hammerdb_vm_postgres', 'hammerdb_kata_postgres',
                                                          'hammerdb_pod_mssql', 'hammerdb_vm_mssql', 'hammerdb_kata_mssql',
-                                                         'vdbench_pod', 'vdbench_kata', 'vdbench_vm']
+                                                         'vdbench_pod', 'vdbench_kata', 'vdbench_vm', 'mpi_pod']
         # benchmark-operator workload types
         self._environment_variables_dict['workload_namespaces'] = {
             'stressng': 'benchmark-operator',
